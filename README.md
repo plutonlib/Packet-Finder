20dbd23f21909ac467138fcd55b6efaa4d329fb8
PlutonLib <lum3t2346@gmail.com>
2024-07-23 07:23:39 +0300

    Version 1.0.1

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 
 # Packer-Finder
 
-Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici teknolojilerini tespit eden bir Python programıdır. Bu program, verilen URL'lerde önceden tanımlanmış paketleyici özelliklerini arar ve bulunan URL'leri belirtilen bir dosyaya kaydeder.
+[English](#English) | [Türkçe](#Türkçe)
 
-## Özellikler
+## English
 
-- Dosyadan veya manuel olarak girilen URL listesinden URL'leri kontrol eder.
-- URL'lerin geçerliliğini kontrol eder.
-- Paralel işleme (multi-threading) kullanarak hızlı URL kontrolü sağlar.
-- Belirli ön uç paketleyici teknolojilerini tespit eder:
+Packer-Finder is a Python program that detects frontend bundling technologies used in specified URLs. This program searches for predefined bundling features in the given URLs and saves the found URLs to a specified file.
+
+### Features
+
+- Checks URLs from a file or manually entered URL list.
+- Verifies the validity of URLs.
+- Utilizes multi-threading for fast URL checking.
+- Detects various frontend bundling technologies and platforms:
   - Webpack
   - Vue.js
   - React
@@ -22,12 +26,104 @@ Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici tekn
   - Next.js
   - Nuxt.js
   - Svelte
-- Bulunan URL'leri belirtilen bir dosyaya kaydeder.
+  - jQuery
+  - Ember.js
+  - Backbone.js
+  - Knockout.js
+  - Dojo
+  - Ext.js
+  - Meteor
+  - Aurelia
+  - Polymer
+  - Lit-element
+  - Stencil
+  - Mithril
+  - Marionette
+  - Riot.js
+  - Alpine.js
+  - SproutCore
+  - Enyo
+  - Chaplin
+  - Famous
+  - Frzr
+  - Glimmer
+  - Rivets.js
+  - Derby
+  - Sammy.js
+  - GWT
+  - Qooxdoo
+  - Zino-UI
+  - WTK
+  - Zepto
+  - Highcharts
+  - D3.js
+  - Chart.js
+  - Three.js
+  - Pixi.js
+  - Babylon.js
+  - Phaser
+  - CreateJS
+  - Greensock
+  - GSAP
+  - Anime.js
+  - P5.js
+  - Processing
+  - Paper.js
+  - Zdog
+  - Sprite.js
+  - Two.js
+  - Mo.js
+  - Kute.js
+  - Lottie
+  - Snap.svg
+  - Tailwind
+  - Bootstrap
+  - Foundation
+  - Bulma
+  - Materialize
+  - UIkit
+  - Semantic-UI
+  - Spectre.css
+  - Milligram
+  - PureCSS
+  - Skeleton
+  - Xenforo
+  - WordPress
+  - Drupal
+  - Joomla
+  - Magento
+  - Shopify
+  - PrestaShop
+  - Ghost
+  - Jekyll
+  - Hugo
+  - Hexo
+  - Octopress
+  - Gatsby
+  - Eleventy
+  - Gridsome
+  - Nuxt
+  - Docusaurus
+  - Sapper
+  - Saber
+  - Nextra
+  - Middleman
+  - Brunch
+  - Assemble
+  - Roots
+  - Wintersmith
+  - Metalsmith
+  - Cloudflare
+  - Firebase
+  - Amazon Cloudfront
+  - Akamai
+  - Incapsula
+  - Sucuri
 
-## Gereksinimler
+### Requirements
 
 - Python 3.x
-- Aşağıdaki Python kütüphaneleri:
+- The following Python libraries:
   - requests
   - re (standard library)
   - logging (standard library)
@@ -36,7 +132,204 @@ Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici tekn
   - concurrent.futures (standard library)
   - tqdm
 
-## Kurulum
+### Installation
+
+1. Ensure that Python 3.x is installed on your system.
+2. Install the required Python libraries:
+   ```sh
+   pip install requests tqdm
+   ```
+
+### Usage
+
+#### 1. Checking URLs from a file
+
+1. Create a file named `urls.txt` and write one URL per line.
+2. Run the program and select the file option:
+   ```sh
+   python packer_finder.py
+   ```
+3. Enter the file name (e.g., `urls.txt`).
+4. Enter the name of the file to save the results (e.g., `results.txt`).
+
+#### 2. Manually entering URLs
+
+1. Run the program and select the URL list option:
+   ```sh
+   python packer_finder.py
+   ```
+2. Enter the URLs one by one. Press Enter to finish.
+3. Enter the name of the file to save the results (e.g., `results.txt`).
+
+### Example Usage
+
+#### Checking URLs from a file
+```sh
+python packer_finder.py
+# Welcome message and selection screen
+# Select 1 and enter the file name: urls.txt
+# Enter the name of the file to save the results: results.txt
+```
+
+#### Manually entering URLs
+```sh
+python packer_finder.py
+# Welcome message and selection screen
+# Select 2 and enter the URLs one by one:
+# http://example.com
+# http://anotherexample.com
+# (Press Enter to finish)
+# Enter the name of the file to save the results: results.txt
+```
+
+### Code Explanation
+
+- `read_urls(filename)`: Reads the specified file and returns the URLs.
+- `is_valid_url(url)`: Checks if the URL is valid.
+- `check_url(url)`: Sends a GET request to the given URL and searches for predefined bundling features.
+- Main program:
+  - Prompts the user to select the input type (file or manual URL).
+  - Checks the URLs and saves the detected ones to a file.
+
+### Logging and Error Handling
+
+- The program logs errors and significant events.
+- Invalid URLs and access errors are logged.
+
+### License
+
+This project is licensed under the MIT License. For more information, see the LICENSE file.
+
+## Türkçe
+
+Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici teknolojilerini tespit eden bir Python programıdır. Bu program, verilen URL'lerde önceden tanımlanmış paketleyici özelliklerini arar ve bulunan URL'leri belirtilen bir dosyaya kaydeder.
+
+### Özellikler
+
+- Dosyadan veya manuel olarak girilen URL listesinden URL'leri kontrol eder.
+- URL'lerin geçerliliğini kontrol eder.
+- Paralel işleme (multi-threading) kullanarak hızlı URL kontrolü sağlar.
+- Çeşitli ön uç paketleyici teknolojilerini ve platformları tespit eder:
+  - Webpack
+  - Vue.js
+  - React
+  - Angular
+  - Parcel
+  - Gulp
+  - Grunt
+  - Rollup
+  - Babel
+  - Browserify
+  - Next.js
+  - Nuxt.js
+  - Svelte
+  - jQuery
+  - Ember.js
+  - Backbone.js
+  - Knockout.js
+  - Dojo
+  - Ext.js
+  - Meteor
+  - Aurelia
+  - Polymer
+  - Lit-element
+  - Stencil
+  - Mithril
+  - Marionette
+  - Riot.js
+  - Alpine.js
+  - SproutCore
+  - Enyo
+  - Chaplin
+  - Famous
+  - Frzr
+  - Glimmer
+  - Rivets.js
+  - Derby
+  - Sammy.js
+  - GWT
+  - Qooxdoo
+  - Zino-UI
+  - WTK
+  - Zepto
+  - Highcharts
+  - D3.js
+  - Chart.js
+  - Three.js
+  - Pixi.js
+  - Babylon.js
+  - Phaser
+  - CreateJS
+  - Greensock
+  - GSAP
+  - Anime.js
+  - P5.js
+  - Processing
+  - Paper.js
+  - Zdog
+  - Sprite.js
+  - Two.js
+  - Mo.js
+  - Kute.js
+  - Lottie
+  - Snap.svg
+  - Tailwind
+  - Bootstrap
+  - Foundation
+  - Bulma
+  - Materialize
+  - UIkit
+  - Semantic-UI
+  - Spectre.css
+  - Milligram
+  - PureCSS
+  - Skeleton
+  - Xenforo
+  - WordPress
+  - Drupal
+  - Joomla
+  - Magento
+  - Shopify
+  - PrestaShop
+  - Ghost
+  - Jekyll
+  - Hugo
+  - Hexo
+  - Octopress
+  - Gatsby
+  - Eleventy
+  - Gridsome
+  - Nuxt
+  - Docusaurus
+  - Sapper
+  - Saber
+  - Nextra
+  - Middleman
+  - Brunch
+  - Assemble
+  - Roots
+  - Wintersmith
+  - Metalsmith
+  - Cloudflare
+  - Firebase
+  - Amazon Cloudfront
+  - Akamai
+  - Incapsula
+  - Sucuri
+
+### Gereksinimler
+
+- Python 3.x
+- Aşağıdaki Python kütüphaneleri:
+  - requests
+  - re (standart kütüphane)
+  - logging (standart kütüphane)
+  - ssl (standart kütüphane)
+  - urllib (standart kütüphane)
+  - concurrent.futures (standart kütüphane)
+  - tqdm
+
+### Kurulum
 
 1. Python 3.x'in sisteminizde kurulu olduğundan emin olun.
 2. Gerekli Python kütüphanelerini yükleyin:
@@ -44,9 +337,9 @@ Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici tekn
    pip install requests tqdm
    ```
 
-## Kullanım
+### Kullanım
 
-### 1. Dosyadan URL Okuma
+#### 1. Dosyadan URL Okuma
 
 1. `urls.txt` adında bir dosya oluşturun ve her satıra bir URL yazın.
 2. Programı çalıştırın ve dosya seçeneğini seçin:
@@ -56,7 +349,7 @@ Packer-Finder, belirli URL'lerde kullanılan ön uç (frontend) paketleyici tekn
 3. Dosya adını girin (örneğin: `urls.txt`).
 4. Sonuçların kaydedileceği dosya adını girin (örneğin: `results.txt`).
 
-### 2. Manuel Olarak URL Girişi
+#### 2. Manuel Olarak URL Girişi
 
 1. Programı çalıştırın ve URL listesi seçeneğini seçin:
    ```sh
@@ -86,7 +379,7 @@ python packer_finder.py
 # Sonuçların kaydedileceği dosya adını girin: results.txt
 ```
 
-## Kodun Açıklaması
+### Kodun Açıklaması
 
 - `read_urls(filename)`: Belirtilen dosyayı okuyarak URL'leri döner.
 - `is_valid_url(url)`: URL'nin geçerli olup olmadığını kontrol eder.
@@ -95,11 +388,11 @@ python packer_finder.py
   - Kullanıcıdan giriş türünü (dosya veya manuel URL) seçmesini ister.
   - URL'leri kontrol eder ve tespit edilenleri bir dosyaya kaydeder.
 
-## Loglama ve Hata Yönetimi
+### Loglama ve Hata Yönetimi
 
 - Program, loglama ile hataları ve önemli olayları kaydeder.
 - Geçersiz URL'ler ve erişim hataları loglanır.
 
-## Lisans
+### Lisans
 
 Bu proje MIT Lisansı ile lisanslanmıştır. Daha fazla bilgi için LICENSE dosyasına bakın.
--- packet-finder.py
+++ packet-finder.py
@@ -1,54 +1,82 @@
 import requests
 import re
 import logging
-import ssl
 import urllib.parse
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
+import os
 
 logging.basicConfig(level=logging.INFO)
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 # Dosyadan URL adreslerini oku
 def read_urls(filename):
-    with open(filename, "r") as f:
-        for line in f:
-            yield line.strip()
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                yield line.strip()
+    except FileNotFoundError:
+        logging.error(f"Dosya bulunamadı: {filename}")
+        exit(1)
 
 # URL'nin geçerli olup olmadığını kontrol et
 def is_valid_url(url):
-    # URL'yi ayrıştır
     parts = urllib.parse.urlparse(url)
-    # HTTP veya HTTPS protokolü olup olmadığını kontrol et
-    return parts.scheme in ['http', 'https'] and parts.netloc
+    return parts.scheme in ['http', 'https', 'file'] and parts.netloc or parts.path
 
-# URL'de ön uç paketleyici özelliği olup olmadığını kontrol et
+# URL veya dosyada ön uç paketleyici özelliği olup olmadığını kontrol et
 def check_url(url):
     try:
-        # GET isteği gönder ve yanıtı bekle
-        response = requests.get(url, timeout=5, verify=False)
-        # Tüm olası özellikleri kontrol et
-        for pattern in patterns:
-            if re.search(pattern, response.text, re.IGNORECASE):
-                # Özellik bulundu, URL ve bulunan özellik ile geri dön
-                logging.info(f"Bu URL'de ön uç paketleyici özelliği bulundu, URL: {url}, Özellik: {pattern}")
-                return url, pattern
+        logging.info(f"URL kontrol ediliyor: {url}")
+        parts = urllib.parse.urlparse(url)
+        if parts.scheme in ['http', 'https']:
+            response = requests.get(url, timeout=10, verify=False)
+            logging.info(f"URL yanıt kodu: {response.status_code}")
+            logging.info(f"Yanıt metni (ilk 500 karakter): {response.text[:500]}")
+
+            if response.status_code == 200:
+                return search_patterns(url, response.text, response.headers)
+            else:
+                logging.warning(f"URL {url} beklenmedik yanıt kodu aldı: {response.status_code}")
+        elif parts.scheme == 'file':
+            path = parts.path
+            if os.path.exists(path):
+                with open(path, 'r') as file:
+                    content = file.read()
+                    return search_patterns(url, content, {})
+            else:
+                logging.error(f"Dosya bulunamadı: {path}")
     except requests.exceptions.RequestException as e:
-        # İstek sırasında hata oluştu, hata logunu kaydet ve None dön
         logging.error(f"URL {url} erişimi başarısız: {e}")
-        return None, None
+    return None, None
+
+# İçeriği kontrol edip desenleri arayan fonksiyon
+def search_patterns(url, content, headers):
+    found_patterns = []
+    for pattern, pattern_type in patterns.items():
+        if pattern_type == 'body':
+            if re.search(pattern, content, re.IGNORECASE):
+                logging.info(f"Özellik bulundu: {pattern}")
+                found_patterns.append(pattern)
+        elif pattern_type == 'header':
+            for header, value in headers.items():
+                if re.search(pattern, value, re.IGNORECASE):
+                    logging.info(f"Özellik bulundu (header): {pattern}")
+                    found_patterns.append(pattern)
+    if found_patterns:
+        return url, ', '.join(found_patterns)
+    return None, None
 
 if __name__ == '__main__':
     print("Packer-Finder'a hoş geldiniz")
     print("Lütfen giriş türünü seçin:")
     print("1. Dosya")
     print("2. URL listesi")
-    # Kullanıcının seçimini oku
     choice = input("Seçiminizi girin (sayı olarak):")
-    # Kullanıcı dosya seçtiyse
     if choice == "1":
         filename = input("Dosya adını girin:")
-        urls = read_urls(filename)
-    # Kullanıcı URL listesi seçtiyse
+        urls = list(read_urls(filename))
+        logging.info(f"Toplam {len(urls)} URL yüklendi.")
     elif choice == "2":
         urls = []
         while True:
@@ -58,40 +86,58 @@ if __name__ == '__main__':
             if is_valid_url(url):
                 urls.append(url)
             else:
-                # URL geçersizse, uyarı logunu kaydet
                 logging.warning(f"Geçersiz URL adresi: {url}")
+        logging.info(f"Toplam {len(urls)} URL yüklendi.")
     else:
-        # Geçersiz seçim yapıldıysa, hata logunu kaydet ve çık
         logging.error("Geçersiz seçim")
         exit(1)
 
-    # Tüm olası ön uç paketleyici özellikleri
-    patterns = ["webpack", "vue", "react", "angular", "parcel", "gulp", "grunt", "rollup", "babel", "browserify", "next.js", "nuxt.js", "svelte"]
+    patterns = {
+        "webpack": "body", "vue": "body", "react": "body", "angular": "body",
+        "parcel": "body", "gulp": "body", "grunt": "body", "rollup": "body",
+        "babel": "body", "browserify": "body", "next.js": "body", "nuxt.js": "body",
+        "svelte": "body", "jquery": "body", "ember.js": "body", "backbone.js": "body",
+        "knockout.js": "body", "dojo": "body", "ext.js": "body", "meteor": "body",
+        "aurelia": "body", "polymer": "body", "lit-element": "body", "stencil": "body",
+        "mithril": "body", "marionette": "body", "riot.js": "body", "alpine.js": "body",
+        "sproutcore": "body", "enyo": "body", "chaplin": "body", "famous": "body",
+        "frzr": "body", "glimmer": "body", "rivets.js": "body", "derby": "body",
+        "sammy.js": "body", "gwt": "body", "qooxdoo": "body", "zino-ui": "body",
+        "wtk": "body", "zepto": "body", "highcharts": "body", "d3.js": "body",
+        "chart.js": "body", "three.js": "body", "pixi.js": "body", "babylon.js": "body",
+        "phaser": "body", "createjs": "body", "greensock": "body", "gsap": "body",
+        "anime.js": "body", "p5.js": "body", "processing": "body", "paper.js": "body",
+        "zdog": "body", "sprite.js": "body", "two.js": "body", "mo.js": "body",
+        "kute.js": "body", "lottie": "body", "snap.svg": "body", "tailwind": "body",
+        "bootstrap": "body", "foundation": "body", "bulma": "body", "materialize": "body",
+        "uikit": "body", "semantic-ui": "body", "spectre.css": "body", "milligram": "body",
+        "purecss": "body", "skeleton": "body", "xenforo": "body", "wordpress": "body",
+        "drupal": "body", "joomla": "body", "magento": "body", "shopify": "body",
+        "prestashop": "body", "ghost": "body", "jekyll": "body", "hugo": "body",
+        "hexo": "body", "octopress": "body", "gatsby": "body", "eleventy": "body",
+        "gridsome": "body", "nuxt": "body", "docusaurus": "body", "sapper": "body",
+        "saber": "body", "nextra": "body", "middleman": "body", "brunch": "body",
+        "assemble": "body", "roots": "body", "wintersmith": "body", "metalsmith": "body",
+        "cloudflare": "header", "firebase": "header", "amazon cloudfront": "header",
+        "akamai": "header", "incapsula": "header", "sucuri": "header"
+    }
 
-    # Tüm URL'leri otomatik olarak kontrol et ve ön uç paketleyici özelliği içerip içermediğini kontrol et
     hit_urls = []
     with ThreadPoolExecutor(max_workers=10) as executor:
         future_to_url = {executor.submit(check_url, url): url for url in urls}
-        # Tüm iş parçacıklarının sonuçlarını gez
         for future in tqdm(future_to_url):
             try:
-                # İş parçacığının sonucunu al
                 hit_url, hit_pattern = future.result()
                 if hit_url is not None:
-                    hit_urls.append(hit_url)
-                    # Ön uç paketleyici özelliği bulunan URL ve özelliği kaydet
+                    hit_urls.append(f"{hit_url} -> {hit_pattern}")
                     logging.info(f"URL {hit_url} ön uç paketleyici özelliği içeriyor: {hit_pattern}")
             except requests.exceptions.Timeout as e:
-                # İstek zaman aşımına uğradıysa, hata logunu kaydet
                 logging.error(f"URL {future_to_url[future]} zaman aşımına uğradı, atlanıyor: {e}")
             except Exception as e:
-                # İş parçacığı sonucu işlenirken hata oluştuysa, hata logunu kaydet
                 logging.error(f"URL {future_to_url[future]} işlenirken hata oluştu, atlanıyor: {e}")
 
-    # Ön uç paketleyici özelliği içeren URL'leri dosyaya kaydet
     output_filename = input("Sonuçları kaydetmek için dosya adını girin:")
     with open(output_filename, "w") as f:
         for url in hit_urls:
             f.write(url + "\n")
-    # Sonuç dosyasının yolunu göster
     logging.info(f"URL adresleri dosyaya kaydedildi: {output_filename}")
